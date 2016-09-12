e7982b1b1b0e9e467bbe181b732f183385353def
IncWolf <incwolf@gmail.com>
2016-09-12 22:28:25 +0300

    init hw 20

--- Lesson_21/Classwork/task2/jsx/main.jsx
+++ Lesson_21/Classwork/task2/jsx/main.jsx
@@ -1,3 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Timer = React.createClass({
+    getInitialState: function () {
+        return {
+            counter: 0
+        }
+    },
+    step: function() {
+        this.setState({counter: this.state.counter+1});
+    },
+    componentDidMount: function () {
+        this.interval = setInterval(this.step, 1000);
+    },
+    componentWillUnmount: function(){
+        clearInterval(this.interval);
+    },
+    render: function () {
+        return (
+            <span>{this.state.counter}</span>
+        )
+    }
+});
+ReactDOM.render(<Timer />, document.getElementById('output'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     entry: {"Lesson_20/Homework/task1/build/bundle": "./Lesson_20/Homework/task1/jsx/main.jsx",
         "Lesson_20/Homework/task2/build/bundle": "./Lesson_20/Homework/task2/jsx/main.jsx",
         "Lesson_20/Homework/task3/build/bundle": "./Lesson_20/Homework/task3/jsx/main.jsx",
-        "Lesson_21/Classwork/task1/build/bundle": "./Lesson_21/Classwork/task1/jsx/main.jsx"},
+        "Lesson_21/Classwork/task1/build/bundle": "./Lesson_21/Classwork/task1/jsx/main.jsx",
+        "Lesson_21/Classwork/task2/build/bundle": "./Lesson_21/Classwork/task2/jsx/main.jsx"},
     output: {
         filename: "[name].js"
     },
